0037b1dd5fa0beff312fab269f8b53fc7cba5376
ivan-tretyak <das.blackya@gmail.com>
2020-05-05 17:19:32 +0300

    add 4 report
    Signed-off-by: ivan-tretyak <das.blackya@gmail.com>

--- Python Files/Cвязанный список/main.py
+++ Python Files/Cвязанный список/main.py
@@ -3,7 +3,7 @@ from linked_list import Linked_List
 def polynomial(a, x, n):
     if a >= n:
         P = Linked_List()
-        for i in range(n, -1, -1):
+        for i in range(n, 0, -1):
             node = a * x ** i
             P.add(node)
             a -= 1
